15ba52533b6d3117db34f5c816efef6bf076320d
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-08-27 16:31:38 -0700

    Added: web resource to filter courses so only the currently active institution's courses are displayed in Student Course History Course Field drop-down

--- Web-Resources/cfasms_filtercourses.js
+++ Web-Resources/cfasms_filtercourses.js
@@ -1,16 +1,20 @@
 function onStudentChange(executionContext) {
     const formContext = executionContext.getFormContext();
-    filterCourses(formContext).then({
-    });
+    filterCourses(formContext);
 }
 
 async function filterCourses(formContext) {
     const studentLookup = formContext.getAttribute("cfasms_student").getValue();
-    if (!studentLookup) return; // Exit if no student is selected
+    if (!studentLookup) return;
 
     const studentId = studentLookup[0].id;
+    const institutionId = await fetchInstitutionId(studentId);
+    const fetchXmlForCourses = buildFetchXmlForCourses(institutionId);
 
-    // Step 1: Fetch the institution ID where the student is currently enrolled
+    setCustomCourseView(formContext, fetchXmlForCourses);
+}
+
+async function fetchInstitutionId(studentId) {
     const fetchXmlForInstitution = [
         "<fetch top='1'>",
         "<entity name='cfasms_studenteducationhistory'>",
@@ -24,10 +28,11 @@ async function filterCourses(formContext) {
     ].join("");
 
     const institutionResult = await Xrm.WebApi.retrieveMultipleRecords("cfasms_studenteducationhistory", "?fetchXml=" + fetchXmlForInstitution);
-    const institutionId = institutionResult.entities[0]._cfasms_institution_value;
+    return institutionResult.entities[0]._cfasms_institution_value;
+}
 
-    // Step 2: Fetch the courses available in the institution
-    const fetchXmlForCourses = [
+function buildFetchXmlForCourses(institutionId) {
+    return [
         "<fetch>",
         "<entity name='cfasms_courses'>",
         "<attribute name='cfasms_coursesid' />",
@@ -38,24 +43,21 @@ async function filterCourses(formContext) {
         "</entity>",
         "</fetch>"
     ].join("");
-    // const coursesToDisplay = Xrm.WebApi.retrieveMultipleRecords("cfasms_studenteducationhistory", "?fetchXml=" + fetchXmlForCourses);
-    const courseControl = formContext.getControl("cfasms_course");
+}
 
-    // Step 3: Set the available options for the cfasms_course lookup field
-    // Step 3: Create Custom View
+function setCustomCourseView(formContext, fetchXmlForCourses) {
+    const courseControl = formContext.getControl("cfasms_course");
     const viewId = "{29483495-0FB7-46BD-955D-90836AEAB3B1}"; // Generate a new GUID
     const entityName = "cfasms_courses";
     const viewDisplayName = "Filtered Courses";
-    const layoutXmlArray = [
+    const layoutXml = [
         "<grid name='resultset' object='1' jump='cfasms_coursesid' select='1' icon='1' preview='1'>",
         "<row name='result' id='cfasms_coursesid'>",
         "<cell name='cfasms_coursename' width='300' />",
         "</row>",
         "</grid>"
-    ];
-    const layoutXml = layoutXmlArray.join("");
+    ].join("");
 
     courseControl.addCustomView(viewId, entityName, viewDisplayName, fetchXmlForCourses, layoutXml, true);
     courseControl.setDefaultView(viewId);
-
 }
