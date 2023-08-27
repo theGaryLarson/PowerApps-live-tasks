2e79eac46ac7f71c594bb2526cb4a0fcc5f1bac3
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-08-27 16:51:15 -0700

    Added: docstrings

--- Web-Resources/filterSubGrid.js
+++ Web-Resources/filterSubGrid.js
@@ -1,10 +1,35 @@
-// Entry point for the script
+/**
+ * Entry point function that runs on tab state change of the Institution main form.
+ *
+ * @param {Object} executionContext - The execution context object.
+ *
+ * It does the following:
+ *
+ * 1. Gets the form context from the executionContext.
+ * 2. Calls the handleSubgrid() function, passing it the formContext.
+ *
+ * This will start the logic to handle filtering the subgrid on form load.
+ */
 function onLoad(executionContext) {
     const formContext = executionContext.getFormContext();
     handleSubgrid(formContext);
 }
 
-// Handle subgrid logic
+/**
+ * Handles logic to filter the student subgrid control.
+ *
+ * @param {Object} formContext - The form context object.
+ *
+ * It does the following:
+ *
+ * 1. Gets the student subgrid control from the formContext.
+ *
+ * 2. Checks if the subgrid is null:
+ *    - If null, waits 2 seconds and retries filtering the subgrid. This handles async load.
+ *    - If not null, calls filterStudentSubgrid() passing formContext and subgrid.
+ *
+ * This allows retrying the filter if the subgrid hasn't loaded yet.
+ */
 function handleSubgrid(formContext) {
     const subgrid = formContext.getControl("universitystudents_sg");
 
@@ -16,7 +41,24 @@ function handleSubgrid(formContext) {
     }
 }
 
-// Fetch and apply filters to the subgrid
+/**
+ * Fetches and applies filters to the student subgrid.
+ *
+ * @param {Object} formContext - The form context object.
+ * @param {Object} subgrid - The student subgrid control.
+ *
+ * It does the following:
+ *
+ * 1. Calls fetchStudentIds() to get the list of student IDs for the institution.
+ *
+ * 2. Passes the student IDs to buildFetchXmlForStudents() to build the FetchXML filter.
+ *
+ * 3. Sets the filter on the subgrid with setFilterXml().
+ *
+ * 4. Refreshes the subgrid to apply the filter.
+ *
+ * This will filter the subgrid to only show students for the current institution.
+ */
 async function filterStudentSubgrid(formContext, subgrid) {
     const studentIds = await fetchStudentIds(formContext);
     const fetchXmlForStudents = buildFetchXmlForStudents(studentIds);
@@ -25,7 +67,25 @@ async function filterStudentSubgrid(formContext, subgrid) {
     subgrid.refresh();
 }
 
-// Fetch student IDs based on institution ID
+/**
+ * Fetches the student IDs for the given institution.
+ *
+ * @param {Object} formContext - The form context object.
+ *
+ * It does the following:
+ *
+ * 1. Gets the institution ID from the form context.
+ *
+ * 2. Calls buildFetchXmlForStudentHistory() to build the FetchXML.
+ *
+ * 3. Executes a Web API retrieveMultipleRecords request with the FetchXML.
+ *
+ * 4. Maps the returned entities to extract just the student ID values.
+ *
+ * 5. Returns the array of student IDs.
+ *
+ * @returns {Array} The array of student ID values for the institution.
+ */
 async function fetchStudentIds(formContext) {
     const institutionId = formContext.data.entity.getId();
     const fetchXml = buildFetchXmlForStudentHistory(institutionId);
@@ -41,7 +101,19 @@ async function fetchStudentIds(formContext) {
     }
 }
 
-// Build FetchXML for student history
+/**
+ * Builds a FetchXML query to get student history records for an institution.
+ *
+ * @param {string} institutionId - The institution ID to filter on.
+ *
+ * It does the following:
+ *
+ * 1. Sets the table as cfasms_studenteducationhistory.
+ * 2. Retrieves the cfasms_student attribute.
+ * 3. Adds a filter for the cfasms_institution equal to the institutionId.
+ *
+ * @returns {string} The FetchXML query string.
+ */
 function buildFetchXmlForStudentHistory(institutionId) {
     return [
         "<fetch version='1.0' output-format='xml-platform' mapping='logical'>",
@@ -55,7 +127,19 @@ function buildFetchXmlForStudentHistory(institutionId) {
     ].join("");
 }
 
-// Build FetchXML for students
+/**
+ * Builds a FetchXML query to filter students by their IDs.
+ *
+ * @param {Array} studentIds - Array of student ID values to filter on.
+ *
+ * It does the following:
+ *
+ * 1. Maps the studentIds to <value> nodes for the IN condition.
+ * 2. Builds a FetchXML query for the cfasms_students table.
+ * 3. Adds a filter condition for cfasms_studentsid IN the student IDs.
+ *
+ * @returns {string} The FetchXML query string.
+ */
 function buildFetchXmlForStudents(studentIds) {
     const studentIdFilters = studentIds.map(id => `<value>${id}</value>`).join("");
     return [
